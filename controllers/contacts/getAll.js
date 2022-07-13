d59695f16a64008dbad0be191d08804b9c60155e
Aleksandr-Pod <stdfire@gmail.com>
2022-07-13 17:44:30 +0300

    Update getAll.js
    don't show createdAt - updatedAt

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -4,11 +4,11 @@ const Contact = require('../../models/newContacts')
 const getAll = async (req, res, next) => {
   try {
     const { _id } = req.user
-    const { page = 1, limit = 100, favorite } = req.query
+    const { page = 1, limit = 100, favorite = false } = req.query
     console.log('favorite:', favorite)
     const skip = (page - 1) * limit
     let result = await Contact
-      .find({ owner: _id }, "", { skip, limit: +limit })
+      .find({ owner: _id }, {createdAt: 0, updatedAt: 0}, { skip, limit: +limit })
       .populate("owner", "_id email")
     if (favorite) {
       result = result.filter(item => item.favorite === true)
