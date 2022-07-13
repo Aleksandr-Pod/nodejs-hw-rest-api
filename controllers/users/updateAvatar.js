cac7d9c00327d8468a4d4998ef8d95d94e61b909
Aleksandr-Pod <stdfire@gmail.com>
2022-07-14 01:29:26 +0300

    Update updateAvatar.js

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -5,13 +5,14 @@ const Jimp = require('jimp')
 
 const patchAvatar = async (req, res) => {
     const { path: tempPath, originalname } = req.file
-    const avatarPath = path.join(__dirname, "../../", "public", "avatars", originalname)
+    const newAvatarName =  `${req.user._id}-${originalname}`
+    const avatarPath = path.join(__dirname, "../../", "public", "avatars", newAvatarName)
     Jimp.read(tempPath)
-        .then(img => img.resize(256, 256).greyscale().write(avatarPath))
+        .then(img => img.resize(250, 250).write(avatarPath))
         .catch(console.log)
     try {
         await fs.unlink(tempPath)
-        const avatarURL = path.join("public", "avatars", originalname)
+        const avatarURL = path.join("public", "avatars", newAvatarName)
         await User.findByIdAndUpdate(req.user._id, { avatarURL })
         res.status(200).json({
             message: "avatar img updated",
