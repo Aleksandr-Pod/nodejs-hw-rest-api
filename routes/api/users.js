6f8e8ce64d5f92b6e588f803edbd55d067411305
Aleksandr-Pod <stdfire@gmail.com>
2022-07-14 01:24:06 +0300

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,5 +9,5 @@ router.post('/login', ctrl.login)
 router.get('/logout', auth, ctrl.logout)
 router.get('/current', auth, ctrl.getCurrent)
 router.patch('/subscription', auth, ctrl.updateSubscr)
-router.patch('/avatar', auth, upload.single('image'), ctrl.updateAvatar)
+router.patch('/avatar', auth, upload.single('avatar'), ctrl.updateAvatar)
 module.exports = router
